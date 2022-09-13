c250117032e0121bfb394c943bea0c1c369df7e0
pater8715 <pater8715@gmail.com>
2022-09-13 08:44:32 -0500

    Actividad finalizada

--- main.ts
+++ main.ts
@@ -1,3 +1,57 @@
-let numero = 10
-numero = numero
-basic.showNumber(numero)
+let value_dice = 0
+input.onGesture(Gesture.Shake, function () {
+    value_dice = randint(1, 6)
+    show_value(value_dice)
+})
+// Función para leer los valores numéricos y representarlos en un dado.
+function show_value (núm: number) {
+    if (núm == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (núm == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (núm == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (núm == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (núm == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (núm == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+}
