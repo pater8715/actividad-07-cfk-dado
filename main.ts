7367eeb7afb14b396d9203d2c49981e5e76c11a2
pater8715 <pater8715@gmail.com>
2022-10-06 08:49:31 -0500

    Taller encuesta

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-let value_dice = 0
-input.onGesture(Gesture.Shake, function () {
-    value_dice = randint(1, 6)
-    show_value(value_dice)
+input.onButtonPressed(Button.AB, function () {
+    a = 0
+    b = 1
+    contador = 0
 })
 // Función para leer los valores numéricos y representarlos en un dado.
 function show_value (núm: number) {
@@ -55,3 +55,28 @@ function show_value (núm: number) {
             `)
     }
 }
+let value_dice = 0
+let contador = 0
+let b = 0
+let a = 0
+a = 1
+b = 0
+contador = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) && (a == 1 && contador < 10)) {
+        value_dice = randint(1, 6)
+        show_value(value_dice)
+        contador += 1
+    } else if (contador == 10) {
+        basic.pause(5000)
+        basic.showIcon(IconNames.Happy)
+    }
+    if (input.buttonIsPressed(Button.B) && (b == 1 && contador < 10)) {
+        value_dice = randint(1, 6)
+        show_value(value_dice)
+        contador += 1
+    } else if (contador == 10) {
+        basic.pause(5000)
+        basic.showIcon(IconNames.Happy)
+    }
+})
